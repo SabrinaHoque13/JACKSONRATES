ed509f034644ccdcb085340e579b5dc2571ffa8e
SabrinaHoque13 <55740348+SabrinaHoque13@users.noreply.github.com>
2019-11-08 10:28:09 +0000

    Added test object

---
+++ TestObject.java
@@ -0,0 +1,56 @@
+package com.spartaglobal;
+
+import static org.junit.Assert.assertTrue;
+
+import com.spartaglobal.HTTPServices.FixerHTTPServices;
+import com.spartaglobal.ParsingJSON.CommentsDTO;
+import com.spartaglobal.ParsingJSON.CommentsDeserialiser;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestObject
+
+{
+//    private static CommentsDeserialiser comments;
+//    static FixerHTTPServices fixerHTTPServices = new FixerHTTPServices();
+//
+//    @BeforeClass
+//    public static void setup() {
+//        fixerHTTPServices.executeAlbumsGetRequest();
+//        comments = new CommentsDeserialiser(fixerHTTPServices.getFixerCommentsJSONString());
+//
+//    }
+//    @Test
+//    public void testResponse()
+//    {
+//        System.out.println(fixerHTTPServices.getFixerCommentsJSONString());
+//
+//    }
+//        public void getindexcount(){
+//            int count = 0;
+////            for (int i =0; i < comments.commentsMapped.length(); i++) {
+////                count++;
+////            }
+//            System.out.println(comments.commentsMapped.length);
+//        }
+//        @Test
+//    public void test1(){
+//
+//        getindexcount();
+
+
+
+//    @Test
+//    public void getEmail() {
+//        for (CommentsDTO Comments : comments.CommentsMapped
+//             ) {
+//            System.out.println(Comments.getEmail());
+//
+//        }
+//    }
+
+
+}
